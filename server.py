47f8a1a267372e7b971b58efd6fd544baa861993
ubdussamad <ubdussamad@gmail.com>
2018-05-10 16:36:15 +0530

    Removed Redundant Methods and modified Documentation.

--- server.py
+++ server.py
@@ -4,13 +4,14 @@ from types import FunctionType
 from functools import wraps
 
 tokens = {}
-master = 0
-credentials_data = {'ubdussamad' : ['5544781558847ecc54e3b3c406ed1c0e',True] ,
-                    'makshuf' : ['ccee66ac39ce8f6f4f2c450679f90525',False]      }
+credentials_data = {'ubdussamad' : ['5544781558847ecc54e3b3c406ed1c0e',True] , #Dummy Credentials for
+                    'makshuf' : ['ccee66ac39ce8f6f4f2c450679f90525',False]  }  #Client-Side Testing
+host  =("192.168.1.102", 8000)
 def wrapper(method):
     @wraps(method)
     def wrapped(*args, **kwrds):
-        print("gaylord")
+        print("NotImplimented")
+        #TODO Further pre method processing block
     return wrapped
 
 class MetaClass(type):
@@ -18,7 +19,7 @@ class MetaClass(type):
         newClassDict = {}
         for attributeName, attribute in classDict.items():
             if isinstance(attribute, FunctionType):
-                # replace it with a wrapped version
+                # should replace it with a wrapped version
                 attribute = wrapper(attribute)
             newClassDict[attributeName] = attribute
         return type.__new__(meta, classname, bases, newClassDict)
@@ -29,7 +30,7 @@ class utils:
         self.key = 0x00000
         
     def login(self,usr,pwd):
-        global master
+        
         credentials = 1 if usr in credentials_data else None
         if credentials and hashlib.md5(pwd.encode('utf-8')).hexdigest() == credentials_data[usr][0] :
             
@@ -47,17 +48,11 @@ class utils:
         * 1,1 - Token Valid and Admin
         * 1,0 - Token Valid but Non-Admin
         * 0,0 = Token Expired or Invalid and Non Admin/Admin 
-        * NO AUTHORITY OF ADMIN IF HIS/HER TOKEN EXPIRED
+        * NO AUTHORITY OF ADMIN IF ADMIN'S TOKEN EXPIRED
         '''
-        for token in tokens:
-            if time.time() - tokens[token][0] > 20:
-                del tokens[token]
 
-
-                #FAULT ABOVE
-                
         if token in tokens:
-            delta = time.time() - tokens[token][0] < 20 #1 Minute(s)
+            delta = time.time() - tokens[token][0] < 60 #1 Minute(s)
             if delta:
                 if tokens[token][1]:
                     return 1,1
@@ -69,6 +64,7 @@ class utils:
         return doc
         
     def list_tokens(self,token):
+        
         if all(self.check_token(token)):
             return(tokens)
         return("Acess Denied! Non-Eligible or Expired Token.")
@@ -76,11 +72,18 @@ class utils:
 doc = '''This is a XML-RPC based Client Server for general method calls with high efficiency and low hastle.
 \n Copyright 2018 BachmanitY Inc.'''
 
-server = SimpleXMLRPCServer(("192.168.1.102", 8000))
+server = SimpleXMLRPCServer(host)
 DocXMLRPCServer.set_server_title(server,server_title='BachmanitY Inc. Servers')
 DocXMLRPCServer.set_server_name(server,server_name='Client Server Network BachmanitY Inc.')
 DocXMLRPCServer.set_server_documentation(server,server_documentation=doc)
 
+print('Bach-RPC Server Running on host: %s and Port: %s'%host)
+print('Logging is enabled , server started at: %s. \n'%(time.ctime(),))
+print('Hit Ctrl + C anytime to hault the server.\n')
 server.register_introspection_functions()
 server.register_instance( utils() )
-server.serve_forever()
+
+try:
+    server.serve_forever()
+except KeyboardInterrupt:
+    print("Shutting Down the server.")
