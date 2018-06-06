007b53efb419d150786faaf9123f9a73836280ac
ubdussamad <ubdussamad@gmail.com>
2018-06-06 13:06:46 +0530

    Updated Stuff

--- C/secure_db_handler.c
+++ C/secure_db_handler.c
@@ -4,7 +4,7 @@
 #include <string.h>
 
 bool comp();
-short load();
+int load();
 short strlength();
 char * strslice();
 void slice_str();
@@ -12,59 +12,26 @@ void slice_str();
 
 int main(int argc, char **argv){
 	//TODO: Impliment line by line checking system
-    load(argv[1]);
-    return(0);
+    
+    char out[50];
+
+    load(argv[1],&out);
+
+    printf("%s", out);
+
+    return 0;
 }
 
 
-
-
-
-short load(char query[])
-{
-
-    FILE * fp;
-    char * line = NULL;
-    size_t len = 0;
-    ssize_t read;
-
-    fp = fopen("/home/samad/bach-rpc/C/s9.db", "r");
-    if (fp == NULL)
-        exit(EXIT_FAILURE);
-    //printf("The argument is:%s \n\n",query);
-    for ( short i = 0 ; (read = getline(&line, &len, fp))  != -1  ; i++) {
-        //printf("\n%d. %s", i, line);
-
-        short ref_i = 0;
-
-        for (short j=0; 1 ; j++){
-
-
-            if ( line[j] == ',' ){
-                ref_i = j;
-                break;
-            }
-        } 
-        
-        char buffer[ref_i+1];
-
-        slice_str( line , buffer , 0 , ref_i-1 );
-
-        //printf("\n  Index: %s and Query is: %s \n", buffer ,query);
-
-        //printf("\n %s \n",buffer);
-
-        if ( strcmp( query , buffer ) == 0) {
-            printf("\n User Exists \n");
-        }
-    }
-
-    fclose(fp);
-    if (line)
-        free(line);
-    exit(EXIT_SUCCESS);
-}
-
+int load (char query[], char *ch) {
+  FILE *fp;char *line = NULL;size_t len = 0;ssize_t read;
+  fp = fopen ("/home/samad/bach-rpc/C/asrar.txt", "r");if (fp == NULL){exit (EXIT_FAILURE);}
+  for (short i = 0; (read = getline (&line, &len, fp)) != -1; i++){short ref_i = 0;
+  for (short j = 0; 1; j++){if (line[j] == ','){ref_i = j;break;}}char buffer[ref_i + 1];
+  slice_str (line, buffer, 0, ref_i - 1);if (strcmp (query, buffer) == 0){int end = strlength (line);
+  char temp[end - ref_i + 1];slice_str (line, &temp, ref_i + 1, end); strcpy(ch,temp);
+  fclose (fp); if (line) { free (line); } return 0; } } strcpy(ch,"Bad User."); fclose (fp);
+  if (line) {free (line);} return 1; }
 
 bool strcomp( char s1[] , char s2[] ) {
     if ( strlength(s1)  != strlength(s2)) {return(false);}
@@ -82,15 +49,9 @@ char * strslice(char ch[] , int initial_index , int final_index ){
     for ( short i = initial_index ; i < final_index ; i++) {
         slice[counter] = ch[i]; counter++; }
     char *string = slice;
-    return( string );
-}
+    return( string ); }
 
-
-void slice_str(const char * str, char * buffer, size_t start, size_t end)
-{
+void slice_str(const char * str, char * buffer, size_t start, size_t end) {
     size_t j = 0;
     for ( size_t i = start; i <= end; ++i ) {
-        buffer[j++] = str[i];
-    }
-    buffer[j] = 0;
-}
+    buffer[j++] = str[i]; } buffer[j] = 0; }
